80df7a9c155090895cd8a6a708a0ec4d7b23694c
purvapatil5 <114835645+purvapatil5@users.noreply.github.com>
2022-12-05 20:18:39 -0800

    Adding Logout shared pref

--- Lifestyle_Management/Lifestyle_Management/app/src/main/java/com/example/lifestyle_management/LandingPage.java
+++ Lifestyle_Management/Lifestyle_Management/app/src/main/java/com/example/lifestyle_management/LandingPage.java
@@ -8,11 +8,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LandingPage extends AppCompatActivity {
     CardView date_card, break_card, routine_card;
+    Button log_out;
+    SharedPreferences sp;
 
 
     @Override
@@ -22,14 +28,15 @@ public class LandingPage extends AppCompatActivity {
 
         date_card =  findViewById(R.id.date_card);
         break_card = findViewById(R.id.breaks_card);
+        log_out = findViewById(R.id.logOut);
 
         DatabaseHelper databasehelper = new DatabaseHelper(LandingPage.this);
         boolean doesTableExist = databasehelper.doesTableExist("BREAKS_TABLE");
         if(!doesTableExist){
             databasehelper.createBreaksTable();
-            databasehelper.addBreak("Work Break", "2022-12-1","2:30 pm", 121,1);
-            databasehelper.addBreak("Gym Break", "2022-12-9","6:30 am", 162,0);
-            databasehelper.addBreak("Water Break", "2022-11-30","11:30 am", 741,1);
+            databasehelper.addBreak("Work Break", "2022-12-1","2:30 pm", 121);
+            databasehelper.addBreak("Gym Break", "2022-12-9","6:30 am", 162);
+            databasehelper.addBreak("Water Break", "2022-11-30","11:30 am", 741);
        }
 
         date_card.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +53,18 @@ public class LandingPage extends AppCompatActivity {
             public void onClick(View view) {
               openBreaksPage();
 
+            }
+        });
+        log_out.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                sp = getSharedPreferences("login", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("logged", true); // set it to true when the user is logged out
+                editor.putString("Email" , null);
+                editor.apply();
+                startActivity(new Intent(LandingPage.this , MainActivity.class));
             }
         });
     }
